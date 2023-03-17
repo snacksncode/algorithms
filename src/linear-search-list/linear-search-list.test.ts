60a02a4543038b05a146d31670d1c198df623fd5
snacksncode <snacksncode@gmail.com>
2023-03-17 21:15:00 +0100

    test: add tests for `linear-search-list`

---
+++ src/linear-search-list/linear-search-list.test.ts
@@ -0,0 +1,10 @@
+import { expect, test } from "vitest";
+import { linear_search_list } from "./linear-search-list";
+
+test("should return true if number is present in a given list of numbers", () => {
+  expect(linear_search_list([1, 2, 3, 4, 5], 3)).toBe(true);
+});
+
+test("should return false if number is not present in a given list of numbers", () => {
+  expect(linear_search_list([1, 2, 3, 4, 5], 10)).toBe(false);
+});

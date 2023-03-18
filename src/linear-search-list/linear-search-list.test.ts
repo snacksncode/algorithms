618b1d0e3b24e5008d466cf271298ce0279d5984
snacksncode <snacksncode@gmail.com>
2023-03-18 09:42:34 +0100

    feat: make `linear-search-list` work with strings and numbers

--- src/linear-search-list/linear-search-list.test.ts
+++ src/linear-search-list/linear-search-list.test.ts
@@ -1,7 +1,7 @@
 import { expect, test } from "vitest";
 import { linear_search_list } from "@/linear-search-list";
 
-test("linear_search_list works correctly", () => {
+test("linear_search_list works correctly with numbers", () => {
   const array = [1, 3, 4, 69, 71, 81, 90, 99, 420, 1337, 69420];
 
   expect(linear_search_list(array, 69)).toEqual(true);
@@ -10,4 +10,15 @@ test("linear_search_list works correctly", () => {
   expect(linear_search_list(array, 69421)).toEqual(false);
   expect(linear_search_list(array, 1)).toEqual(true);
   expect(linear_search_list(array, 0)).toEqual(false);
+});
+
+test("linear_search_list works correctly with strings", () => {
+  const array = ["a", "b", "c", "d", "e", "f"];
+
+  expect(linear_search_list(array, "a")).toEqual(true);
+  expect(linear_search_list(array, "A")).toEqual(false);
+  expect(linear_search_list(array, "b")).toEqual(true);
+  expect(linear_search_list(array, "B")).toEqual(false);
+  expect(linear_search_list(array, "f")).toEqual(true);
+  expect(linear_search_list(array, "F")).toEqual(false);
 });
--- src/linear-search-list/linear-search-list.ts
+++ src/linear-search-list/linear-search-list.ts
@@ -1,6 +1,6 @@
 // Runtime complexity of linear search is O(n)
 
-export function linear_search_list(array: number[], value: number): boolean {
+export function linear_search_list<T extends string | number>(array: T[], value: T): boolean {
   for (let i = 0; i < array.length; i++) {
     if (array[i] === value) {
       return true;

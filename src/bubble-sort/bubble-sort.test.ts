4e10d8fb31381ec8ea77cc44ee3b32ecc2c1ba47
snacksncode <snacksncode@gmail.com>
2023-03-18 09:20:13 +0100

    test: add tests for `bubble-sort`

---
+++ src/bubble-sort/bubble-sort.test.ts
@@ -0,0 +1,9 @@
+import { expect, test } from "vitest";
+import { bubble_sort } from "./bubble-sort";
+
+test("bubble-sort does the thing", function () {
+  const arr = [9, 3, 7, 4, 69, 420, 42];
+
+  bubble_sort(arr);
+  expect(arr).toEqual([3, 4, 7, 9, 42, 69, 420]);
+});

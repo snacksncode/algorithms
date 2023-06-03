a8830370391d35f16a937c083b9a1e1a459bade5
snacksncode <snacksncode@gmail.com>
2023-06-03 20:26:24 +0200

    test: add tests for `queue`

---
+++ src/queue/queue.test.ts
@@ -0,0 +1,26 @@
+import { Queue } from "@/queue";
+import { expect, test } from "vitest";
+
+test("queue", function () {
+  const queue = new Queue<number>();
+
+  queue.enqueue(5);
+  queue.enqueue(7);
+  queue.enqueue(9);
+
+  expect(queue.deque()).toEqual(5);
+  expect(queue.length).toEqual(2);
+
+  queue.enqueue(11);
+
+  expect(queue.deque()).toEqual(7);
+  expect(queue.deque()).toEqual(9);
+  expect(queue.peek()).toEqual(11);
+  expect(queue.deque()).toEqual(11);
+  expect(queue.deque()).toEqual(undefined);
+  expect(queue.length).toEqual(0);
+
+  queue.enqueue(69);
+  expect(queue.peek()).toEqual(69);
+  expect(queue.length).toEqual(1);
+});
